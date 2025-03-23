98a5de6e52680a924d0fc98cfaf073bf98f39e4c
Dhiego <diegotorremacetas@gmail.com>
2025-03-23 18:46:26 -0500

    First Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login.component';
+import { TasksListComponent } from './features/tasks/pages/tasks-list/tasks-list.component';
+import { HomeComponent } from './layout/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'tasks', component: TasksListComponent },
+  { path: '**', redirectTo: '' }, // Ruta por defecto
+];
